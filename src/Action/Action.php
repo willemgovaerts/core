ba0cac77d40826f1dfb01b1088b01754c24ec0c2
karan jilka <karan.jilka@levaral.com>
2018-02-02 20:10:26 +0530

    Add action route helper

---
+++ src/Action/Action.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Levaral\Core\Action;
+
+use Illuminate\Support\Facades\Route;
+
+class Action
+{
+    public static function get($url, $ActionClass)
+    {
+        Route::get($url, $ActionClass)
+            ->name(self::getName($ActionClass));
+    }
+
+    public static function post($url, $ActionClass)
+    {
+        Route::post($url, $ActionClass)
+            ->name(self::getName($ActionClass));
+    }
+
+    protected static function getName($ActionClass)
+    {
+        $reflector = new \ReflectionClass($ActionClass);
+        $fullPath = $reflector->getFileName();
+        $name = ltrim(substr($fullPath, strpos($fullPath, "Actions")), 'Actions/');
+        $name = rtrim($name, '.php');
+
+        return str_replace('/', ':', $name);
+    }
+}
