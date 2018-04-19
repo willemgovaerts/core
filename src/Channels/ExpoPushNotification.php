c2580497697d2a5b605a6d88393f3cb8fbd88f21
karan jilka <karan.jilka@levaral.com>
2018-04-19 19:17:03 +0530

    Add exception throw

--- src/Channels/ExpoPushNotification.php
+++ src/Channels/ExpoPushNotification.php
@@ -8,6 +8,7 @@ use GuzzleHttp\Client;
 class ExpoPushNotification
 {
     const EXPO_PUSH_URL = 'https://exp.host/--/api/v2/push/send';
+    const SUCCESS_STATUS_CODE = 200;
 
     /**
      * Send the given notification.
@@ -31,12 +32,20 @@ class ExpoPushNotification
 
         // Send notification to the $notifiable instance...
         $client = new Client();
+
         $request = $client->post(self::EXPO_PUSH_URL, [
-            'accept' => 'application/json',
-            'accept-encoding' => 'gzip, deflate',
-            'content-type' => 'application/json',
+            'headers' => [
+                'accept' => 'application/json',
+                'accept-encoding' => 'gzip, deflate',
+                'content-type' => 'application/json',
+            ],
+            'json' => $content
         ], $content);
 
         $request->send();
+
+        if($request->getStatusCode() !== SUCCESS_STATUS_CODE) {
+            throw new \Exception('Could not process expo push notification request');
+        }
     }
 }
